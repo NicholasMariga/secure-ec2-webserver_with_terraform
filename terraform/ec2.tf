175dd2918f4c2109f4d547207124c0c50c6ee5ee
Nicholas Mariga <dolphnikoz@gmail.com>
2025-11-21 17:37:39 +0300

    Updated terraform to destroy

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -59,9 +59,3 @@ resource "aws_instance" "web" {
     Name = "${var.project}-web"
   }
 }
-
-
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
